29d796baa2affd38da0a24aa751c016af5148fa5
Alan Chalmers <alan.chalmers@gmail.com>
2014-03-29 00:14:03 +1100

    Changed to use wget instead of Curl so this will work on Solaris10

--- scripts/solaris/postinstall.sh
+++ scripts/solaris/postinstall.sh
@@ -6,8 +6,11 @@ echo "Setting the vagrant ssh pub key"
 mkdir /export/home/vagrant/.ssh
 chmod 700 /export/home/vagrant/.ssh
 touch /export/home/vagrant/.ssh/authorized_keys
-curl -sL http://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub > \
-    /export/home/vagrant/.ssh/authorized_keys
+if [ -f /usr/sfw/bin/wget ] ; then
+  /usr/sfw/bin/wget --no-check-certificate http://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub -O /export/home/vagrant/.ssh/authorized_keys
+else
+  /usr/bin/wget --no-check-certificate http://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub -O /export/home/vagrant/.ssh/authorized_keys
+fi
 chmod 600 /export/home/vagrant/.ssh/authorized_keys
 chown -R vagrant:staff /export/home/vagrant/.ssh
 
@@ -16,7 +19,7 @@ echo "Disabling sendmail and asr-norify"
 /usr/sbin/svcadm disable sendmail
 /usr/sbin/svcadm disable asr-notify
 
-echo "Clearing log files and zeroing disk, while take a while"
+echo "Clearing log files and zeroing disk, this might take a while"
 cp /dev/null /var/adm/messages
 cp /dev/null /var/log/syslog
 cp /dev/null /var/adm/wtmpx
